bbf5e46acf1c5c4fa7635193026cffc523088e57
milligosh <adjubelmillicent@gmail.com>
2024-06-18 09:48:13 +0000

    fixed-validation

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,38 +1,37 @@
-const  { responseProvider }  = require('../helper/response');
-import {Request, Response, NextFunction} from 'express'
+const { responseProvider } = require("../helper/response");
+import { Request, Response, NextFunction } from "express";
 
-
-
-export const validateSignUpApplicantInput = (req:Request, res:Response, next:NextFunction) => {
-
+export const validateSignUpApplicantInput = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const { email, firstname, lastname,username, password, phonenumber } = req.body;
+    const { email, firstname, lastname, username, password, phonenumber } =
+      req.body;
 
-
-    if (typeof email !== 'string' || !email.includes('@')) {
-      return responseProvider( res, null, 'provide a valid email', 400)
+    if (typeof email !== "string" || !email.includes("@")) {
+      return responseProvider(res, null, "provide a valid email", 400);
     }
 
-    if (typeof firstname !== 'string' || !firstname) {
-      return responseProvider( res, null, 'provide a valid firstname', 400)
+    if (typeof firstname !== "string" || !firstname) {
+      return responseProvider(res, null, "provide a valid firstname", 400);
     }
 
-
-    if (typeof lastname !== 'string' || !lastname) {
-      return responseProvider( res, null, 'provide a valid lastname', 400)
+    if (typeof lastname !== "string" || !lastname) {
+      return responseProvider(res, null, "provide a valid lastname", 400);
     }
-    if (typeof username !== 'string'|| !username){
-        return responseProvider (res ,null,'provide a valid username',400)
-    }
-
-    if (typeof password !== 'string' || password.length < 8) {
-      return responseProvider( res, null, 'provide a valid password', 400)
+    if (typeof username !== "string" || !username) {
+      return responseProvider(res, null, "provide a valid username", 400);
     }
 
-    if (typeof parseInt(phonenumber) !== 'number' || phonenumber.length < 10) {
-      return responseProvider( res, null, 'provide a valid phone number', 400)
+    if (typeof password !== "string" || password.length < 8) {
+      return responseProvider(res, null, "provide a valid password", 400);
     }
 
+    if (typeof parseInt(phonenumber) !== "number" || phonenumber.length < 10) {
+      return responseProvider(res, null, "provide a valid phone number", 400);
+    }
 
     return next();
   } catch (error) {
